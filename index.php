83f03f4261c7b0beec68eed8f04cb038acb140c4
EuroSofts <55707631+eurosofts@users.noreply.github.com>
2020-11-24 09:29:33 +0000

    First Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include("../../config/constantes.php");
+
+if (isset($_POST['id'])) { 
+    include("../../controller/login.php");
+
+    $objLogin = new Login();
+
+    $objLogin->setID($_POST['id']);
+    $id = $objLogin->getID();
+
+    $objLogin->setSenha($_POST['senha']);
+    $senha = $objLogin->getSenha();
+
+    $objLogin->setValidacao($id, $senha );
+    $mensagem = $objLogin->getValidacao();
+
+    if (!empty($mensagem))
+      echo "erro: ".$mensagem;
+    else
+      echo "tudo ok";  
+}
+else {
+
+    include("../../view/login.html"); 
+
+}
